11a8c7881df0c6717598e477709c40ba3aec3e86
Benjamin5607 <86463042+Benjamin5607@users.noreply.github.com>
2026-01-16 17:25:50 +0800

    Update base_miner.dart

--- ​lib/miners/base_miner.dart
+++ ​lib/miners/base_miner.dart
@@ -1,8 +1,9 @@
-// 모든 마이너들의 아버지
+// lib/miners/base_miner.dart
+
 abstract class BaseMiner {
-  // 1. 검색 기능: 검색어를 주면 날것의 텍스트(Raw Data) 리스트를 뱉어라.
+  /// 검색어(query)를 받아서 원본 데이터(Raw Data) 리스트를 반환합니다.
   Future<List<String>> mine(String query);
-  
-  // 2. 마이너 이름 (디버깅용: "지금 네이버가 일하는 중..." 확인)
+
+  /// 어떤 광부인지 이름표 (디버깅용)
   String get minerName;
 }
